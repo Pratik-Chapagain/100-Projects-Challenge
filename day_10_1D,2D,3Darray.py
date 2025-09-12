f6bf94be5f0e412bb831092d0050be5c6879f0e2
Pratik Chapagain <chapagainpratik28@gmail.com>
2025-09-12 22:09:52 +0545

    arrays practice numpy

---
+++ day_10_1D,2D,3Darray.py
@@ -0,0 +1,20 @@
+#1d-array
+
+import numpy as np
+arr_1d = np.array([10,20,30,40,50])
+
+print(arr_1d)
+
+
+#2d-array
+arr_2d = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+
+print(arr_2d)
+
+
+#multidimensional-array
+matrix = np.array([[2,4,6],
+                   [8,10,12]])
+print(matrix)
