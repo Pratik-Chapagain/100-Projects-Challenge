469f9413f9e461aa7f0a38951e4614db847d392c
Pratik Chapagain <chapagainpratik28@gmail.com>
2025-09-24 23:12:48 +0545

    Advanced Numpy

---
+++ Broadcasting.py/solution.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+prices = np.array([100,200,300,400])
+discount = 10 #scalar
+
+final_prices = prices - (prices * discount/100)
+
+print(final_prices )
+
+
+# no loops required numpy automatically applys discount
---
+++ Vectorization/fast2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10,20,30])
+
+multiplied = arr * 3
+
+print(multiplied)
+
+#Vectorization is very much helpful in deeplearning and Artificial Intelligence while processing large number of data
